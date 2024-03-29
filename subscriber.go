60ddabb8e80ca248fc91203cb2af528507dd1598
Joshua Crane <josh.crane512@gmail.com>
2024-03-29 08:16:53 +0800

    Adding subscriber comments

--- subscriber.go
+++ subscriber.go
@@ -6,19 +6,27 @@ import (
 	"github.com/mr-joshcrane/rivulet/store"
 )
 
+// Subscriber is a consumer of messages. It expects to receive messages
+// from its [Receiver] and save them to its [Store].
 type Subscriber struct {
 	receiver Receiver
 	Store    store.Store
 }
 
+// Receiver is a mechanism for receiving messages.
 type Receiver interface {
 	Receive(context.Context) []Message
 }
 
+// InMemoryReceiver is a Receiver that receives messages from an InMemoryTransport
 type InMemoryReceiver struct {
 	messages <-chan Message
 }
 
+// Receive blocks until a message is available or the context is done.
+// It then returns all messages received up to that point.
+// Ideally signal the context when you're done receiving messages, rather than
+// closing the channel.
 func (r *InMemoryReceiver) Receive(ctx context.Context) []Message {
 	var messages []Message
 	for {
@@ -34,6 +42,8 @@ func (r *InMemoryReceiver) Receive(ctx context.Context) []Message {
 	}
 }
 
+// Receive receives messages from the [Receiver] and saves them to the [Store].
+// It returns an error if there was a problem saving the messages.
 func (s *Subscriber) Receive(ctx context.Context) error {
 	messages := s.receiver.Receive(ctx)
 	var convertedMessages []store.Message
